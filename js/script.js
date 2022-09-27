2dc42cea0c4a4dde9c8aad426791822ed0e9fd18
RafaelDonizetti <rafaeldonizetti20.13@gmail.com>
2022-09-26 22:23:47 -0300

    responsividade aplicada

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('nav-mobile').style.width = "100%"
+    document.getElementById('botao').style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById('nav-mobile').style.width = "0%"
+    document.getElementById('botao').style.display = "block";
+}
